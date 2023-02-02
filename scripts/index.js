dadee1be30d851ce2ffcdfc129ded041a583339f
Leisan Andriyanova <andriyanova.lm@yandex.ru>
2023-02-02 21:39:35 +0300

    Project_4 ver.1.5

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 /*открытие и закрытие popup*/
+
 const editButton = document.querySelector('.profile__edit')
 const popup = document.querySelector('.popup')
 const closeButton = popup.querySelector('.popup__close')
@@ -8,27 +9,32 @@ let jobInput = document.querySelector('.popup__form-item_el_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__text');
 
+const toggleOpenPopup = () => {
+    popup.classList.toggle('popup_opened')
+}
+
 const handleEditButtonClick = () => {
-    popup.classList.add('popup_opened')
+    toggleOpenPopup();
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }    
 
 const handleCloseButtonClick = () => {
-    popup.classList.remove('popup_opened')
+    toggleOpenPopup();
 }
     
 editButton.addEventListener('click', handleEditButtonClick)
 closeButton.addEventListener('click', handleCloseButtonClick)
 
 /*сoхранение данных в формах*/
+
 function handleFormSubmit (evt) {
     evt.preventDefault();
 
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
    
-    popup.classList.remove('popup_opened')
+    toggleOpenPopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
